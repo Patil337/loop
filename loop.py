9edb5d68c4403f98837d090959adb5479b2e133c
Patil337 <88849833+Patil337@users.noreply.github.com>
2022-06-22 11:21:54 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,25 @@
+#while loop:
+i=1
+while i<=10:
+    print(i)
+    i=i+1
+#list with loop:
+l1=['mango','apple','chikoo','cherry']
+for i in l1:
+    print(i) 
+#2.       
+l2=['black','red','blue']
+l3=['chair','table','book']
+for i in l2:
+    for j in l3:
+        print(i,j)
+#3.
+i=2
+while(i<=5):
+    if(i<3):
+        print(i+1)
+    else:
+        print(i-1)
+    i=i+1        
+
+   
